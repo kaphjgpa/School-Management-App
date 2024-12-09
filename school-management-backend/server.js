8f650d5a032dbdfe9dffc3da1d65a472f70a9a2c
Surendra singh <surendrasinghc80@gmail.com>
2024-12-09 15:08:26 +0530

    cors new url

--- school-management-backend/server.js
+++ school-management-backend/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 // Enable CORS
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend's URL
+    origin: "https://cuvette-lpcv.onrender.com", // Replace with your frontend's URL
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow credentials like cookies or authorization headers
