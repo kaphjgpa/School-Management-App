10debc8c5617308a7be6cc3d30d50bf89e38eeb9
Surendra singh <surendrasinghc80@gmail.com>
2024-12-12 19:10:27 +0530

    minnor fix

--- school-management-frontend/components/DashboardContent.jsx
+++ school-management-frontend/components/DashboardContent.jsx
@@ -10,7 +10,7 @@ export function DashboardContent() {
       <h1 className="text-3xl font-semibold text-gray-900 dark:text-white">
         Dashboard
       </h1>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         <AdminUpdateDialog />
         <AssignClass />
         <CreateClass />
